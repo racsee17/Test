ff0e7fd1b723e271eb86042e214ec95c92334635
racsee17 <76049948+racsee17@users.noreply.github.com>
2025-08-08 16:34:38 +0800

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-
-function App() {
-  return <h1>Hello from Inventory Tracker</h1>;
-}
-
-createRoot(document.getElementById("root")).render(<App />);
